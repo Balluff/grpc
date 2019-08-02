2c775b2193f5a5945f991231f922bd44b173d292
Stephen Bryant <stephen.bryant@balluff.de>
2019-08-02 16:07:15 +0200

    Added CPack functionality for generating RPM or DEB packages
    ie: run `cpack -G RPM` (or "DEB") after building with CMake.
    
    The current configuration produces a development package, and a shared library
    package when CMake is configured with -DBUILD_SHARED_LIBS=ON.
    
    Other changes are:
    - use GNUInstallDirs to generate correct paths (ie: "lib64" on some systems)
    - set the VERSION property on libraries
    
    Testing status:
    - RPM tested on OpenSUSE Leap 15.1
    - DEB tested on Ubuntu Core 19.04
    - NSIS (Windows/setup.exe) not tested, but might work anyway.

---
+++ cmake/cpack.cmake
@@ -0,0 +1,90 @@
+#
+# Packaging
+# https://cmake.org/cmake/help/latest/module/CPack.html
+#
+
+set( CPACK_PACKAGE_NAME ${PROJECT_NAME} )
+set( CPACK_PACKAGE_VENDOR "Google" )
+set( CPACK_PACKAGE_DESCRIPTION_SUMMARY "gRPC - An RPC library and framework" )
+set( CPACK_PACKAGE_HOMEPAGE_URL "https://grpc.io" )
+set( CPACK_PACKAGE_CONTACT      "https://grpc.io" )
+set( CPACK_PACKAGE_VERSION ${PACKAGE_VERSION} )
+set( CPACK_PACKAGE_INSTALL_DIRECTORY ${PROJECT_NAME} )
+set( CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/LICENSE" )
+set( CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md" )
+
+set( CPACK_COMPONENT_Library_DISPLAY_NAME "gRPC Library" )
+set( CPACK_COMPONENT_Library_DESCRIPTION "The gRPC binary library." )
+set( CPACK_COMPONENT_Library_REQUIRED 1 )
+set( CPACK_COMPONENT_Devel_DISPLAY_NAME "gRPC Development Files" )
+set( CPACK_COMPONENT_Devel_DESCRIPTION "Development files for compiling against gRPC." )
+set( CPACK_COMPONENT_Devel_REQUIRED 0 )
+
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+
+    if ( "${CPACK_PACKAGE_ARCHITECTURE}" STREQUAL "" )
+        # Note: the architecture should default to the local architecture, but it
+        # in fact comes up empty.  We call `uname -m` to ask the kernel instead.
+        EXECUTE_PROCESS( COMMAND uname -m COMMAND tr -d '\n' OUTPUT_VARIABLE CPACK_PACKAGE_ARCHITECTURE )
+    endif()
+
+    set( CPACK_INCLUDE_TOPLEVEL_DIRECTORY 1 )
+    set( CPACK_PACKAGE_RELEASE 1 )
+
+
+    # RPM - https://cmake.org/cmake/help/latest/cpack_gen/rpm.html
+    set( CPACK_RPM_PACKAGE_RELEASE ${CPACK_PACKAGE_RELEASE} )
+    set( CPACK_RPM_PACKAGE_ARCHITECTURE ${CPACK_PACKAGE_ARCHITECTURE} )
+    set( CPACK_RPM_PACKAGE_DESCRIPTION ${CPACK_PACKAGE_DESCRIPTION_SUMMARY} )
+    set( CPACK_RPM_PACKAGE_URL ${CPACK_PACKAGE_HOMEPAGE_URL} )
+    set( CPACK_RPM_PACKAGE_LICENSE "APACHE-2" )
+    set( CPACK_RPM_COMPONENT_INSTALL 1 )
+    set( CPACK_RPM_COMPRESSION_TYPE "xz" )
+    set( CPACK_RPM_PACKAGE_AUTOPROV 1 )
+
+    set( CPACK_RPM_Library_PACKAGE_ARCHITECTURE ${CPACK_PACKAGE_ARCHITECTURE} )
+    set( CPACK_RPM_Library_PACKAGE_NAME "lib${CPACK_PACKAGE_NAME}" )
+    set( CPACK_RPM_Library_FILE_NAME
+         "${CPACK_RPM_Library_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${CPACK_RPM_PACKAGE_RELEASE}.${CPACK_RPM_Library_PACKAGE_ARCHITECTURE}.rpm" )
+    set( CPACK_RPM_Library_PACKAGE_SUMMARY ${CPACK_COMPONENT_Library_DESCRIPTION} )
+
+    set( CPACK_RPM_Devel_PACKAGE_REQUIRES "cmake >= ${CMAKE_MINIMUM_REQUIRED_VERSION}" )
+    set( CPACK_RPM_Devel_PACKAGE_SUMMARY ${CPACK_COMPONENT_Devel_DESCRIPTION} )
+    set( CPACK_RPM_Devel_PACKAGE_ARCHITECTURE ${CPACK_RPM_PACKAGE_ARCHITECTURE} )
+    set( CPACK_RPM_Devel_PACKAGE_NAME "${CPACK_PACKAGE_NAME}-devel" )
+    set( CPACK_RPM_Devel_FILE_NAME
+         "${CPACK_RPM_Devel_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${CPACK_RPM_PACKAGE_RELEASE}.${CPACK_RPM_Devel_PACKAGE_ARCHITECTURE}.rpm" )
+
+    # If no shared libraries are built, all targets land in the "Devel" component.
+    # If there's only one component, the RPM builder reverts to non-component
+    # settings.
+    set( CPACK_RPM_PACKAGE_NAME ${CPACK_RPM_Devel_PACKAGE_NAME} )
+    set( CPACK_RPM_FILE_NAME ${CPACK_RPM_Devel_FILE_NAME} )
+
+
+    # DEB - https://cmake.org/cmake/help/latest/cpack_gen/deb.html
+    set( CPACK_DEBIAN_PACKAGE_RELEASE ${CPACK_PACKAGE_RELEASE} )
+    set( CPACK_DEBIAN_PACKAGE_HOMEPAGE ${CPACK_PACKAGE_HOMEPAGE_URL} )
+    set( CPACK_DEB_COMPONENT_INSTALL 1 )
+    set( CPACK_DEBIAN_COMPRESSION_TYPE "xz")
+
+    if ( ${CPACK_PACKAGE_ARCHITECTURE} STREQUAL "x86_64" )
+        set( CPACK_DEBIAN_PACKAGE_ARCHITECTURE "amd64" )  # DEB doesn't always use the kernel's arch name
+    else()
+        set( CPACK_DEBIAN_PACKAGE_ARCHITECTURE ${CPACK_PACKAGE_ARCHITECTURE} )
+    endif()
+
+    set( CPACK_DEBIAN_FILE_NAME "DEB-DEFAULT" ) # Use default naming scheme
+
+    set( CPACK_DEBIAN_LIBRARY_PACKAGE_NAME "lib${CPACK_PACKAGE_NAME}" )
+    set( CPACK_DEBIAN_LIBRARY_PACKAGE_SHLIBDEPS 1 )
+
+    set( CPACK_DEBIAN_DEVEL_PACKAGE_DEPENDS ${CPACK_RPM_Devel_PACKAGE_REQUIRES} )
+    set( CPACK_DEBIAN_DEVEL_PACKAGE_NAME "${CPACK_PACKAGE_NAME}-dev" )
+
+elseif( ${CMAKE_HOST_WIN32} )
+    set( CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL ON )
+    set( CPACK_NSIS_DISPLAY_NAME ${PROJECT_NAME} )
+    set( CPACK_NSIS_PACKAGE_NAME ${PROJECT_NAME} )
+    set( CPACK_NSIS_URL_INFO_ABOUT ${CPACK_PROJECT_HOMEPAGE_URL} )
+endif()
